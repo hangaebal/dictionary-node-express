f8d7ecf0cbfdf0cde425e53832fb1c77d2158dd9
hangaebal <hangaebal@gmail.com>
2016-06-14 14:31:23 +0900

    dictionary2
    - 네이버 사전 API 적용

--- routes/dictionary.js
+++ routes/dictionary.js
@@ -1,9 +1,32 @@
 var express = require('express');
 var router = express.Router();
 
+var https = require('https');
+
+var CLIENT_ID = 'HnPF7N0k2aGWefOZoJJA';
+var CLIENT_SECRET = 'I7KFTf9LSS';
+var API_URI = '/v1/search/encyc.xml?query=';
+
+var options = {
+  host: 'openapi.naver.com',
+  port: 443,
+  path: API_URI,
+  method: 'GET',
+  headers: {'X-Naver-Client-Id':CLIENT_ID, 'X-Naver-Client-Secret': CLIENT_SECRET}
+};
+
 router.get('/', function(req, res, next) {
-  res.render('dictionary', {title: 'dic'});
-  
+  var searchText = encodeURIComponent('개발');
+  options.path = API_URI + searchText;
+  var apiReq = https.request(options, function(apiRes) {
+    console.log('STATUS: ' + apiRes.statusCode);
+    apiRes.setEncoding('utf8');
+    apiRes.on('data', function (chunk) {
+      res.setHeader('Content-Type', 'application/xml');
+      res.send(chunk);
+    });
+  });
+  apiReq.end();
 });
 
 module.exports = router;
