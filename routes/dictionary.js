329d644a25b00e82f375059a4d8d5483102a117a
hangaebal <hangaebal@gmail.com>
2016-06-15 17:50:47 +0900

    뷰 적용 및 API 결과 처리

--- routes/dictionary.js
+++ routes/dictionary.js
@@ -5,28 +5,39 @@ var https = require('https');
 
 var CLIENT_ID = '발급받은 ID';
 var CLIENT_SECRET = '발급받은 SECRET';
-var API_URI = '/v1/search/encyc.xml?query=';
+var API_URI = '/v1/search/encyc.xml?display=20&query=';
 
 var options = {
   host: 'openapi.naver.com',
   port: 443,
   path: API_URI,
   method: 'GET',
-  headers: {'X-Naver-Client-Id':CLIENT_ID, 'X-Naver-Client-Secret': CLIENT_SECRET}
+  headers: {'X-Naver-Client-Id': CLIENT_ID, 'X-Naver-Client-Secret': CLIENT_SECRET}
 };
 
 router.get('/', function(req, res, next) {
-  var searchText = encodeURIComponent('개발');
-  options.path = API_URI + searchText;
-  var apiReq = https.request(options, function(apiRes) {
-    console.log('STATUS: ' + apiRes.statusCode);
-    apiRes.setEncoding('utf8');
-    apiRes.on('data', function (chunk) {
-      res.setHeader('Content-Type', 'application/xml');
-      res.send(chunk);
+  if (typeof req.query.q === 'undefined') {
+    res.render('dictionary', {title: 'dic'});
+  } else {
+    var searchText = encodeURIComponent(req.query.q);
+    options.path = API_URI + searchText;
+    var apiReq = https.request(options, function(apiRes) {
+      var statusCode = apiRes.statusCode;
+      console.log('STATUS: ' + statusCode);
+      if (statusCode === 200) {
+        apiRes.setEncoding('utf8');
+        apiRes.on('data', function(chunk) {
+          var parseString = require('xml2js').parseString;
+          parseString(chunk, function(err, result) {
+            res.send(result.rss.channel[0].item);
+          });
+        });
+      } else {
+        res.status(statusCode).send('Naver API error return');
+      }
     });
-  });
-  apiReq.end();
+    apiReq.end();
+  }
 });
 
 module.exports = router;
